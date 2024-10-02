fe5920d7c2e1da96aadd9b69c7acfe15624ddbe2
Edilson <juniorse13@gmail.com>
2024-10-02 14:09:21 -0300

    feat: a custom card component

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, defineProps } from "vue";
+
+const props = defineProps<{
+  title: string;
+  actions: {
+    text: string;
+    color?: string;
+    function: (p: any) => any;
+  }[];
+}>();
+
+const title = ref(props.title);
+const actions = ref(props.actions);
+</script>
+
+<template>
+  <v-card>
+    <v-card-title v-if="title">{{ title }}</v-card-title>
+    <v-card-text>
+      <slot name="content"></slot>
+    </v-card-text>
+    <v-card-actions v-if="actions" v-for="action in actions">
+      <v-btn variant="tonal" :color="action.color" @click="action.function">
+        {{ action.text }}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
