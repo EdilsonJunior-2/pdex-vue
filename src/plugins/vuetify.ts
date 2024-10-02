3cc25075c3652d8c78d5570f609556474dc56261
Edilson <juniorse13@gmail.com>
2024-10-02 14:10:05 -0300

    feat: using vuetify components

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,43 @@
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+
+const configVuetify = {
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+  },
+  theme: {
+    defaultTheme: "myCustomTheme",
+    themes: {
+      myCustomTheme: {
+        colors: {
+          normal: "#A8A77A",
+          fire: "#EE8130",
+          water: "#6390F0",
+          electric: "#F7D02C",
+          grass: "#7AC74C",
+          ice: "#96D9D6",
+          fighting: "#C22E28",
+          poison: "#A33EA1",
+          ground: "#E2BF65",
+          flying: "#A98FF3",
+          psychic: "#F95587",
+          bug: "#A6B91A",
+          rock: "#B6A136",
+          ghost: "#735797",
+          dragon: "#6F35FC",
+          dark: "#705746",
+          steel: "#B7B7CE",
+          fairy: "#D685AD",
+        },
+      },
+    },
+  },
+};
+const vuetify = createVuetify(configVuetify);
+
+export default vuetify;
