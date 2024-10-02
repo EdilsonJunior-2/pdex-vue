bbdfc3263d88db378dc51cc65026baa6995e09f9
Edilson <juniorse13@gmail.com>
2024-10-02 16:37:50 -0300

    feature: main pokemon list at home view

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,62 @@
 <script setup lang="ts">
+import { pokemonList } from "@/api/pokemon";
+import { ref, watch, onBeforeMount } from "vue";
+import Card from "@/components/Card.vue";
+import ListItem from "@/commons/classes/lists/listItem";
+import PokemonListItem from "@/commons/classes/lists/pokemonListItem";
 
+const pokemons = ref<PokemonListItem[]>([]);
+const offset = ref<number>(0);
+const limit = ref<number>(20);
+const total = ref<number>(0);
+const page = ref<number>(1);
+
+const catchEmAll = () =>
+  pokemonList(offset.value, limit.value).then((res: any) => {
+    total.value = res.count;
+    pokemons.value = res.results.map(
+      (poke: ListItem) => new PokemonListItem(poke)
+    );
+  });
+
+watch(page, (curr, _) => {
+  offset.value = limit.value * (curr - 1);
+  catchEmAll();
+});
+
+onBeforeMount(() => {
+  catchEmAll();
+});
 </script>
 
 <template>
-  <h1>Home</h1>
+  <main>
+    <v-container>
+      <v-pagination v-model="page" :length="total / limit"></v-pagination>
+      <v-row justify="space-between">
+        <v-col cols="4" v-for="(pokemon, index) in pokemons">
+          <Card
+            :title="`${pokemon.name} #${pokemon.number}`"
+            :actions="[
+              {
+                text: 'details',
+                function: () => false,
+              },
+            ]"
+          >
+            <template v-slot:content>
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                  offset + index + 1
+                }.png`"
+                :alt="(pokemon as any).name"
+              />
+            </template>
+          </Card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "@/pages/Home.vue";
-import About from "@/pages/About.vue";
-const routes = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/about", name: "About", component: About },
-];
+
+const routes = [{ path: "/", name: "Home", component: Home }];
 
 const router = createRouter({
   history: createWebHistory(),
