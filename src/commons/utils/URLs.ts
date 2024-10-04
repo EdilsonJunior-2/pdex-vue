9d7c6f9eaade269ef5817e0fd90e7adf2f27aed8
Edilson <juniorse13@gmail.com>
2024-10-03 21:04:32 -0300

    feat: saving type sprites base URL

--- src/commons/utils/URLs.ts
+++ src/commons/utils/URLs.ts
@@ -1,2 +1,4 @@
 export const sprite = (pokemonIndex: number | string) =>
   `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
+export const typeSprite = (typeIndex: number | string) =>
+  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/${typeIndex}.png`;
