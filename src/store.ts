08c0c2ca59e736215c9147d186f98266898898a8
Edilson <juniorse13@gmail.com>
2024-10-04 19:59:33 -0300

    fix: fixing the import names for the modules

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,10 @@
 import { createStore } from "vuex";
-import pokemonModule from "./modules/pokemonList/store";
-import typeListModule from "./modules/typeList/store";
+import pokemonModule from "./modules/pokemon/store";
+import typeModule from "./modules/type/store";
 
 export default createStore({
   modules: {
     pokemonModule,
-    typeListModule,
+    typeModule,
   },
 });
