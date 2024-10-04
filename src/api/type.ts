c47f93f40096b1709499de1a07fb276b61a227d3
Edilson <juniorse13@gmail.com>
2024-10-03 22:02:55 -0300

    fix: now the List creation is in the api getter function

--- src/api/type.ts
+++ src/api/type.ts
@@ -1,14 +1,15 @@
 import { ListItemInterface } from "@/commons/interfaces/lists/list";
 import api from ".";
 import { Type } from "@/commons/classes/type";
+import ListItem from "@/commons/classes/lists/listItem";
 
 export const getTypeList = () =>
   api
     .get(`type?offset=0&limit=100`)
-    .then(
-      (res: { data: { results: ListItemInterface[]; count: number } }) =>
-        res.data
-    );
+    .then((res: { data: { results: ListItemInterface[]; count: number } }) => ({
+      count: res.data.count,
+      results: res.data.results.map((type) => new ListItem(type)),
+    }));
 
 export const getType = (typeIndex: string | number): Promise<Type> =>
   api.get(`type/${typeIndex}`).then((res) => new Type(res.data));
