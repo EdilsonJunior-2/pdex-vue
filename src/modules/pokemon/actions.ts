fae7c5063a04db51e09a50cdb49fa97e434f2968
Edilson <juniorse13@gmail.com>
2024-10-06 02:01:18 -0300

    feat: fetching a selected pokemon

--- src/modules/pokemon/actions.ts
+++ src/modules/pokemon/actions.ts
@@ -1,4 +1,7 @@
-import { getPokemonList } from "@/api/pokemon";
+import { getPokemonList, pokemonData } from "@/api/pokemon";
+import Pokemon from "@/commons/classes/pokemon";
+import { PokemonInterface } from "@/commons/interfaces/pokemon";
+import { PokemonSpeciesInterface } from "@/commons/interfaces/pokemonSpecies";
 const actions = {
   fetchPokemonList(
     { commit }: any,
@@ -7,6 +10,20 @@ const actions = {
     getPokemonList(params.offset, params.limit).then((res) => {
       commit("setPokemonList", res);
     });
+  },
+  fetchPokemon({ commit }: any, params?: { id: number }) {
+    params
+      ? pokemonData(params.id).then(
+          (res: {
+            pokemon: PokemonInterface;
+            pokemon_species: PokemonSpeciesInterface;
+          }) =>
+            commit(
+              "setSelectedPokemon",
+              new Pokemon({ ...res.pokemon, ...res.pokemon_species })
+            )
+        )
+      : commit("setSelectedPokemon");
   },
 };
 
